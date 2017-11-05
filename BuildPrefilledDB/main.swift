7dc7f473f454753e6170c17823a97efc07a5723d
Kazutaka Homma <special18fender@gmail.com>
2017-11-04 21:58:24 -0700

    Initial Commit

---
+++ BuildPrefilledDB/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BuildPrefilledDB
+//
+//  Created by Kazutaka Homma on 11/4/17.
+//  Copyright © 2017 Kazutaka Homma. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
